fcc31e822757619e2705bcc9e2679c72d0b3a634
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-20 17:02:43 +0200

    chore: config index.ts

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './translations/i18n';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import '../presentation/styles/global.scss';
+import "../presentation/styles/global.scss";
 
-import { Routes } from '@/presentation/router';
-import { GlobalDataProvider } from '@/store/contexts';
+import { Routes } from "@/presentation/router";
 
-const container = document.getElementById('main');
+const container = document.getElementById("main");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <GlobalDataProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </GlobalDataProvider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>
 );
