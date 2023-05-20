0f44428989fb05cf0f8b98d4f2e5f7b3277a412b
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-20 18:12:10 +0200

    chore: config app to works with svg as component

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,26 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/main/index.tsx',
+  entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'main-bundle-[hash].js',
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "main-bundle-[hash].js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    extensions: [".ts", ".tsx", ".js", ".scss"],
     alias: {
-      '@': path.join(__dirname, 'src'),
+      "@": path.join(__dirname, "src"),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
   },
   plugins: [new CleanWebpackPlugin()],
 };
