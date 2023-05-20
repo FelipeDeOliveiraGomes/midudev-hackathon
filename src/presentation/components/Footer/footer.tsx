9e5afbb3fe77a880f3aaccb3e571faad9ff58711
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-20 20:15:29 +0200

    feat: adds footer component

---
+++ src/presentation/components/Footer/footer.tsx
@@ -0,0 +1,13 @@
+import { AppLogo } from "@/presentation/components";
+
+import "./footer.scss";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="app-footer">
+      <AppLogo />
+    </footer>
+  );
+};
+
+export default Footer;
