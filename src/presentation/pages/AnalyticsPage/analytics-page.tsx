b1a60bb5a5d7b17ef1caf890fdd5927a24e96456
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-26 22:14:32 +0200

    feat: add analytics-page

---
+++ src/presentation/pages/AnalyticsPage/analytics-page.tsx
@@ -0,0 +1,4 @@
+const AnalyticsPage: React.FC = () => {
+  return <div className="analytics-page"></div>;
+};
+export default AnalyticsPage;
--- src/presentation/pages/index.tsx
+++ src/presentation/pages/index.tsx
@@ -1,1 +1,2 @@
 export { default as HomePage } from "./HomePage/home-page";
+export { default as AnalyticsPage } from "./AnalyticsPage/analytics-page";
