e6a59e7d3d840647d6452ff9511a62473449719c
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-27 01:31:18 +0200

    feat: adds new links to app

--- src/presentation/components/Header/header-utils.ts
+++ src/presentation/components/Header/header-utils.ts
@@ -1,16 +1,24 @@
 import { routesRecord } from "@/presentation/router/routes-record";
 
 export const linksToRender = [
-  {
-    path: routesRecord.ANALYTICS,
-    label: "Analiticas",
-  },
-  {
-    path: routesRecord.JOB_ASSISTANT,
-    label: "Job Assistant",
-  },
-  {
-    path: routesRecord.CURRICULUM_ANALISER,
-    label: "Analisador de Curriculos",
-  },
+    {
+        path: routesRecord.ANALYTICS,
+        label: "Analíticas",
+    },
+    {
+        path: routesRecord.CURRICULUM_ANALISER,
+        label: "Analisis De Curriculum",
+    },
+    {
+        path: routesRecord.SALARY_COMPARISSON,
+        label: "Comparador De Sueldos",
+    },
+    {
+        path: routesRecord.JOB_ASSISTANT,
+        label: "Job Assistant",
+    },
+    {
+        path: routesRecord.ABOUT,
+        label: "Sobre este proyecto",
+    },
 ];
--- src/presentation/router/routes-record.ts
+++ src/presentation/router/routes-record.ts
@@ -1,5 +1,7 @@
 export const routesRecord = {
-  ANALYTICS: "/analytics",
-  JOB_ASSISTANT: "/job-assistant",
-  CURRICULUM_ANALISER: "/curriculum-analiser",
+    ANALYTICS: "/analytics",
+    JOB_ASSISTANT: "/job-assistant",
+    CURRICULUM_ANALISER: "/curriculum-analiser",
+    SALARY_COMPARISSON: "/salary-comparisson",
+    ABOUT: "/about",
 };
