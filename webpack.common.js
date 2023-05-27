e8a6bbc0609ed215218b006bbff9163ca4e419ce
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-28 01:38:30 +0200

    chore: config app to use svg images

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,39 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main/index.tsx",
-  output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "main-bundle-[hash].js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".scss"],
-    alias: {
-      "@": path.join(__dirname, "src"),
+    entry: './src/main/index.tsx',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'main-bundle-[hash].js',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-    ],
-  },
-  plugins: [new CleanWebpackPlugin()],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.scss'],
+        alias: {
+            '@': path.join(__dirname, 'src'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                            publicPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [new CleanWebpackPlugin()],
 };
