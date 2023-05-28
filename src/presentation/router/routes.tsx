e83f8511eeab39aa2dafcd15908545a80dde144b
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-28 15:20:01 +0200

    feat: makes title changes dynamically

--- src/presentation/router/routes.tsx
+++ src/presentation/router/routes.tsx
@@ -1,16 +1,49 @@
-import { useRoutes } from 'react-router-dom';
+import { RouteObject, useLocation, useRoutes } from 'react-router-dom';
 
-import { AnalyticsPage, HomePage, SalaryComparissonPage } from '../pages';
+import {
+    AnalyticsPage,
+    HomePage,
+    JobAssistantPage,
+    SalaryComparissonPage,
+} from '../pages';
 import { routesRecord } from './routes-record';
+import { useEffect } from 'react';
 
-const { ANALYTICS, SALARY_COMPARISSON } = routesRecord;
+const { ANALYTICS, SALARY_COMPARISSON, JOB_ASSISTANT } = routesRecord;
 
-const Routes: React.FC = () =>
-    useRoutes([
-        { path: '/', element: <HomePage /> },
-        { path: ANALYTICS, element: <AnalyticsPage /> },
-        { path: SALARY_COMPARISSON, element: <SalaryComparissonPage /> },
+type AppRoutes = RouteObject & {
+    title?: string;
+};
+
+const Routes: React.FC = () => {
+    const location = useLocation();
+
+    const routes: AppRoutes[] = [
+        { path: '/', element: <HomePage />, title: 'InfoJobs Tools' },
+        {
+            path: ANALYTICS,
+            element: <AnalyticsPage />,
+            title: 'Analytics',
+        },
+        {
+            path: SALARY_COMPARISSON,
+            element: <SalaryComparissonPage />,
+            title: 'Salary Comparisson',
+        },
+        {
+            path: JOB_ASSISTANT,
+            element: <JobAssistantPage />,
+            title: 'Job Assistant',
+        },
         { path: '*', element: <>Not found</> },
-    ]);
+    ];
+
+    useEffect(() => {
+        const title = routes.find((route) => route.path === location.pathname);
+        document.title = title?.title ? title.title : 'InfoJobs Tools';
+    }, [location, routes]);
+
+    return useRoutes(routes);
+};
 
 export default Routes;
