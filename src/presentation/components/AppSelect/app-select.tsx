494a4958f96ef22bca42714be61b1cae9aa623ba
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-28 14:11:46 +0200

    refactors: optimize app-select component

--- src/presentation/components/AppSelect/app-select.tsx
+++ src/presentation/components/AppSelect/app-select.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './app-select.scss';
 
 type AppSelectOptionsType = {
@@ -20,85 +20,95 @@ const AppSelect: React.FC<AppSelectParams> = ({
     onChange = () => {},
 }) => {
     const [searchExp, setSearchExp] = useState('');
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
-    const dropdownRef = useRef<HTMLDivElement>(null);
-    const inputRef = useRef<HTMLInputElement>(null);
+    const appSelectRef = useRef<HTMLDivElement>(null);
 
     const filterOptions = (options: AppSelectOptionsType[]) => {
         const filterRegExp = new RegExp(searchExp, 'gi');
         return options.filter(({ label }) => filterRegExp.test(label));
     };
 
-    const openDropDown = () => {
-        if (!dropdownRef.current || !inputRef.current) return;
-
-        dropdownRef.current!.style.visibility = 'visible';
-        inputRef.current.focus();
+    const closeDropDown = () => {
+        setIsDropdownVisible(false);
     };
 
-    const closeDropDown = () => {
-        if (!dropdownRef.current) return;
-        dropdownRef.current!.style.visibility = 'hidden';
+    const toggleDropdownVisibility = () => {
+        setIsDropdownVisible((prevState) => !prevState);
     };
 
     const optionsFiltered = filterOptions(options);
 
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (
+                appSelectRef.current &&
+                !appSelectRef.current.contains(event.target as Node)
+            ) {
+                closeDropDown();
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
     return (
-        <div className="app-select">
+        <div ref={appSelectRef} className="app-select">
             <span className="app-select__label">{label}</span>
 
-            <div className="app-select__selected-item-display-container">
-                <input
-                    type="text"
-                    className="app-select__selected-item-display"
-                    value={value}
-                    onClick={openDropDown}
-                />
+            <div
+                onClick={toggleDropdownVisibility}
+                className="app-select__selected-item-display-container"
+            >
+                <span className="app-select__selected-item-display">
+                    {value}
+                </span>
             </div>
 
-            <div ref={dropdownRef} className="app-select__dropdown">
-                <input
-                    ref={inputRef}
-                    onBlur={() =>
-                        setTimeout(() => {
-                            closeDropDown();
-                        }, 100)
-                    }
-                    className="app-select__searchbox"
-                    value={searchExp}
-                    onChange={({ target }) => setSearchExp(target.value)}
-                    type="text"
-                />
+            {isDropdownVisible && (
+                <div className="app-select__dropdown">
+                    <input
+                        className="app-select__searchbox"
+                        value={searchExp}
+                        onChange={({ target }) => setSearchExp(target.value)}
+                        type="text"
+                    />
 
-                <ul className="app-select__options-list">
-                    {optionsFiltered.length ? (
-                        optionsFiltered.map((options) => (
-                            <li
-                                className={`app-select__li ${
-                                    options.value === value
-                                        ? 'app-select__li--selected'
-                                        : ''
-                                }`}
-                                onClick={() => {
-                                    onChange(options.value);
-                                }}
-                                key={options.value}
-                            >
-                                {options.label}
-                            </li>
-                        ))
-                    ) : (
-                        <li className="app-select__li">
-                            <span>
-                                No encontrado:
-                                <span className="app-select__highlight">
-                                    "{searchExp}"
+                    <ul className="app-select__options-list">
+                        {optionsFiltered.length ? (
+                            optionsFiltered.map((option) => (
+                                <li
+                                    className={`app-select__li ${
+                                        option.value === value
+                                            ? 'app-select__li--selected'
+                                            : ''
+                                    }`}
+                                    onClick={() => {
+                                        onChange(option.value);
+                                        closeDropDown();
+                                    }}
+                                    key={option.value}
+                                >
+                                    {option.label}
+                                </li>
+                            ))
+                        ) : (
+                            <li className="app-select__li">
+                                <span>
+                                    No encontrado:
+                                    <span className="app-select__highlight">
+                                        "{searchExp}"
+                                    </span>
                                 </span>
-                            </span>
-                        </li>
-                    )}
-                </ul>
-            </div>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
