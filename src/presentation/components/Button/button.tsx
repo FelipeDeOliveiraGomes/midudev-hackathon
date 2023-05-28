17a3b2870a4d36832538b369382d6b5c8523cc00
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-05-28 19:38:34 +0200

    feat: adds disabled styles to button

--- src/presentation/components/Button/button.tsx
+++ src/presentation/components/Button/button.tsx
@@ -4,15 +4,21 @@ export type ButtonProps = {
     children: React.ReactNode;
     onClick?: () => void;
     className?: string;
+    disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
     children,
     onClick,
     className = '',
+    disabled,
 }) => {
     return (
-        <button onClick={onClick} className={`app-button ${className}`}>
+        <button
+            disabled={disabled}
+            onClick={onClick}
+            className={`app-button ${className}`}
+        >
             {children}
         </button>
     );
